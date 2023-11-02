1c780a693b73d5408e1e81865e1f62ba26c307a8
danimart <danimart@student.42madrid.com>
2023-11-02 06:36:19 +0100

    Fix echo builtin

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 03:02:26 by danimart          #+#    #+#             */
-/*   Updated: 2023/11/02 04:56:48 by danimart         ###   ########.fr       */
+/*   Updated: 2023/11/02 06:35:13 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	bin_echo(int fd, char *input)
 {
-	write(fd, input, sizeof(input));
+	write(fd, input, ft_strlen(input));
 	write(fd, "\n", 1);
 	return (0);
 }
