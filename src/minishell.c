d1d02c5675eed0345c8f496772a4acb2cd7ff69e
danimart <danimart@student.42madrid.com>
2023-11-02 05:03:52 +0100

    Added exit code

--- src/minishell.c
+++ src/minishell.c
@@ -6,21 +6,24 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 04:12:22 by danimart          #+#    #+#             */
-/*   Updated: 2023/11/02 04:50:16 by danimart         ###   ########.fr       */
+/*   Updated: 2023/11/02 05:03:28 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	process_input(char *input)
+void	process_input(char *input, int *exit_code)
 {
 	if (startswith(input, "echo "))
-		bin_echo(0, input + 5);
+		*exit_code = bin_echo(0, input + 5);
 }
 
 int	main(void)
 {
+	int	exit_code;
+
+	exit_code = 0;
 	while (TRUE)
-		process_input(readline("minishell > "));
-	return (0);
+		process_input(readline("minishell > "), &exit_code);
+	return (exit_code);
 }
