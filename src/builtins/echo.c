a348af4dd739bfe80a69af8848d2110c71d4a986
danimart <danimart@student.42madrid.com>
2023-11-02 06:43:27 +0100

    echo builtin: Added -n flag

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 03:02:26 by danimart          #+#    #+#             */
-/*   Updated: 2023/11/02 06:35:13 by danimart         ###   ########.fr       */
+/*   Updated: 2023/11/02 06:41:59 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int	bin_echo(int fd, char *input)
 {
-	write(fd, input, ft_strlen(input));
-	write(fd, "\n", 1);
+	if (startswith(input, "-n"))
+		write(fd, input + 3, ft_strlen(input + 3));
+	else
+	{
+		write(fd, input, ft_strlen(input));
+		write(fd, "\n", 1);
+	}
 	return (0);
 }
