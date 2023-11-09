b15c5551cbbda0e295e7581c866893f04fbcc6fe
Ignacio Ortego De La Mata <iortego42madrid@gmail.com>
2023-11-09 13:47:13 +0100

    Primera version final del lexer

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: iortego- <iortego-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 17:30:07 by iortego-          #+#    #+#             */
-/*   Updated: 2023/11/05 12:03:03 by iortego-         ###   ########.fr       */
+/*   Updated: 2023/11/09 13:33:33 by iortego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,19 @@ typedef enum s_type {
 	DOUBLE_QUOTES,
 	OUTPUT_RED,
 	INPUT_RED,
+	SPACE,
+	DOLLAR,
 	PIPE,
 	T_SIZE
 }	t_types;
 
-const static char	*g_token[T_SIZE] = {
+const static char	*g_symbol[T_SIZE] = {
 [SIMPLE_QUOTES] = "'",
 [DOUBLE_QUOTES] = "\"",
 [OUTPUT_RED] = ">",
 [INPUT_RED] = "<",
+[SPACE] = " ",
+[DOLLAR] = "$",
 [PIPE] = "|",
 };
 typedef struct s_symbol
@@ -37,9 +41,20 @@ typedef struct s_symbol
 	int		pos;
 	t_types	type;
 }	t_symbol;
+typedef struct s_syntaxnode
+{
+	t_symbol	symbol;
+	char		*sequence;
+	t_s_node	*next;	
+	t_s_node	*prev;
+}	t_s_node;
 t_symbol	*lexer(const char	*sequence);
+int			count_symbols(const char	*sequence);
 t_types		is_symbol(const char	*sequence);
-t_bool		get_symbols(const char	*sequence, t_symbol	**s);
+int			get_symbols(const char	*sequence, t_symbol	**s);
+
+t_s_node	*parser(const char	*sequence);
+
 
 
 
--- src/parser/lexer.c
+++ src/parser/lexer.c
@@ -6,29 +6,50 @@
 /*   By: iortego- <iortego-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 11:48:58 by iortego-          #+#    #+#             */
-/*   Updated: 2023/11/05 12:13:53 by iortego-         ###   ########.fr       */
+/*   Updated: 2023/11/09 13:44:13 by iortego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-t_symbol	*lexer(const char	*sequence)
+int	count_symbols(const char	*sequence)
 {
-	t_symbol	*sym_l;
-	int			index;
-	int			token_c;
-	t_types		symbol;
+	int		index;
+	int		q_end;
+	int		symbol_c;
+	t_types	symbol;
 
-	token_c = 0;
+	symbol_c = 0;
 	index = 0;
 	while (sequence[index] != 0)
 	{
 		symbol = is_symbol(&sequence[index]);
 		if (symbol != -1)
-			token_c++;
+			symbol_c++;
+		if (symbol == SIMPLE_QUOTES || symbol == DOUBLE_QUOTES)
+		{
+			q_end = is_quote_terminated(&sequence[index]);
+			if (q_end == FALSE)
+				return (-1);
+			index += q_end;
+		}
 		index++;
 	}
-	sym_l = malloc(sizeof(t_symbol) * (token_c + 1));
+	return (index);
+}
+
+t_symbol	*lexer(const char	*sequence)
+{
+	t_symbol	*sym_l;
+	int			index;
+	int			symbol_c;
+	t_types		symbol;
+
+	symbol_c = count_symbols(sequence);
+	if (symbol_c == -1)
+		return (NULL);
+	index = 0;
+	sym_l = malloc(sizeof(t_symbol) * (symbol_c + 1));
 	if (sym_l == NULL)
 		return (NULL);
 	if (get_symbols(sequence, &sym_l))
@@ -37,22 +58,27 @@ t_symbol	*lexer(const char	*sequence)
 	return (NULL);
 }
 
-t_bool	get_symbols(const char	*sequence, t_symbol	**s)
+int	get_symbols(const char	*sequence, t_symbol	**s)
 {
 	int		index;
-	int		token_i;
-	t_types	token;
+	int		symbol_i;
+	t_types	symbol;
 
 	index = 0;
-	token_i = 0;
+	symbol_i = 0;
 	while (sequence[index] != 0)
 	{
-		token = is_symbol(&sequence[index]);
-		if (token != -1)
-			s[0][token_i] = (t_symbol){.type = token, .pos = index};
+		symbol = is_symbol(&sequence[index]);
+		if (symbol != -1)
+		{
+			s[0][symbol_i] = (t_symbol){.type = symbol, .pos = index};
+			symbol_i++;
+		}
+		if (symbol == SIMPLE_QUOTES && symbol == DOUBLE_QUOTES)
+			index += is_quote_terminated(&sequence[index]);
 		index++;
 	}
-	return (TRUE);
+	return (symbol_i);
 }
 
 t_types	is_symbol(const	char	*sequence)
@@ -62,10 +88,28 @@ t_types	is_symbol(const	char	*sequence)
 	index = 0;
 	while (index < T_SIZE)
 	{
-		if (ft_strnstr(sequence, g_token[index],
-				ft_strlen(g_token[index])) != NULL)
+		if (ft_strnstr(sequence, g_symbol[index],
+				ft_strlen(g_symbol[index])) != NULL)
 			return (index);
 		index++;
 	}
 	return (-1);
+}
+
+int	is_quote_terminated(const char	*sequence)
+{
+	int		index;
+	t_types	symbol;
+	char	quote;
+
+	symbol = is_symbol(sequence);
+	quote = g_symbol[symbol][0];
+	index = 1;
+	while (sequence[index] != '\0')
+	{
+		if (sequence[index] == quote)
+			return (index);
+		index++;
+	}
+	return (FALSE);
 }
