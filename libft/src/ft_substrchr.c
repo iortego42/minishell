a418429ce2b0d869dee21ce9426ef013e00ce8a5
danimart <danimart@student.42madrid.com>
2024-03-20 05:17:18 +0100

    Fixed ft_substrchr

--- libft/src/ft_substrchr.c
+++ libft/src/ft_substrchr.c
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 09:00:21 by danimart          #+#    #+#             */
-/*   Updated: 2024/03/07 15:19:19 by danimart         ###   ########.fr       */
+/*   Updated: 2024/03/20 05:16:12 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*ft_substrchr(char *str, char ch, int start)
 {
 	char	*substr;
 	int		end;
+	int		substr_i;
 
 	if (str == NULL)
 		return (NULL);
@@ -24,13 +25,15 @@ char	*ft_substrchr(char *str, char ch, int start)
 		end++;
 	if (start > end)
 		return (NULL);
-	substr = malloc(end);
+	substr = malloc(end * sizeof(char));
 	if (substr == NULL)
 		return (NULL);
+	substr_i = 0;
 	while (start < end)
 	{
-		substr[start] = str[start];
+		substr[substr_i] = str[start];
 		start++;
+		substr_i++;
 	}
 	return (substr);
 }
