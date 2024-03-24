5113406980897ff23adfb3e96292a6215f5c15e4
nachh <iortego42madrid@gmail.com>
2024-03-24 22:28:48 +0100

    Primer Test del tipo de redireccion pasado

--- test/test_srcs/reds.cpp
+++ test/test_srcs/reds.cpp
@@ -8,6 +8,7 @@ TEST_BASE(RedsTest)
 {
     t_string    cmdstr;
     t_cmd       cmd;
+    int         files;
     std::string sentence, testvalue, message;
     
     void setup() {
@@ -27,7 +28,6 @@ TEST_BASE(RedsTest)
 
 TEST_GROUP_BASE(RedsCount, RedsTest)
 {
-    int         e_reds;
     t_string    e_cmdstr; 
 
     void setup() {
@@ -40,7 +40,7 @@ TEST_GROUP_BASE(RedsCount, RedsTest)
         message = sentence + testvalue;
         cmd = get_cmd(cmdstr);
         // for (int i = 0; i < cmd->reds_c; printnl(cmd->reds[i++].filename));
-        CHECK_TEXT(cmd->reds_c == e_reds, message.c_str());
+        CHECK_TEXT(cmd->reds_c == files, message.c_str());
         CHECK_TEXT(str_cmp(e_cmdstr, cmd->cmd) == -1, message.c_str());
         dtor(&e_cmdstr);
         RedsTest::teardown();
@@ -49,7 +49,7 @@ TEST_GROUP_BASE(RedsCount, RedsTest)
 
 TEST(RedsCount, OneRedSQ)
 {
-    e_reds = 1;
+    files = 1;
     ctor(&e_cmdstr, "echo OneRedSQ ");
     testvalue = "echo OneRedSQ >'esto'";
     // testvalue = "echo hola >'esto es una red'";
@@ -57,21 +57,21 @@ TEST(RedsCount, OneRedSQ)
 
 TEST(RedsCount, OneRedDQ)
 {
-    e_reds = 1;
+    files = 1;
     ctor(&e_cmdstr, "echo OneRedDQ ");
     testvalue = "echo OneRedDQ >\"doubequotees\"";
 }
 
 TEST(RedsCount, TwoRedSimple)
 {
-    e_reds = 2;
+    files = 2;
     ctor(&e_cmdstr, "echo TwoRedSimple  ");
     testvalue = "echo TwoRedSimple >>\"doubequotees\" >>EOF";
 }
 
 TEST(RedsCount, MultiRedsMix)
 {
-    e_reds = 4;
+    files = 4;
     ctor(&e_cmdstr, "echo MultiRedsMix    ");
     testvalue = "echo MultiRedsMix >>\"append\" <infile <<heredoc >'hola'";
 }
@@ -84,14 +84,13 @@ TEST_GROUP_BASE(FilenameTest, RedsTest)
 {
     t_string    *e_filename;
     char        **filenames;
-    int         files;
+
     void setup() {
         RedsTest::setup();
         sentence = "Expected Values: ";
     };
 
-    void copyFilenames(const char *filesn[])
-    {
+    void copyFilenames(const char *filesn[]) {
         filenames = (char **)malloc(sizeof(char *) * (unsigned long)(files + 1));
         for (int i = 0; filesn[i]; i++) {
             filenames[i] = (char *)malloc(strlen(filesn[i]) + 1);
@@ -106,8 +105,7 @@ TEST_GROUP_BASE(FilenameTest, RedsTest)
         e_filename = (t_string *)malloc(sizeof(t_string) * (unsigned long)(files + 1));
         for (int i = 0; i < files; ctor(&e_filename[i], filenames[i]), i++);
         e_filename[files] = NULL;
-        for (int i = 0; e_filename[i] != NULL; i++)
-        {
+        for (int i = 0; e_filename[i] != NULL; i++) {
             message = sentence + e_filename[i]->data + "\n" "Value obtained: " + cmd->reds[i].filename->data + "\n";
             CHECK_TEXT(str_cmp(e_filename[i], cmd->reds[i].filename), message.c_str());
         }
@@ -140,4 +138,59 @@ TEST(FilenameTest, Various)
     files = 5;
     testvalue = "echo Various >outputfile1 <<'simpleheredoc' >>\"doubleappend\" <\"doubleinput\" <<heredoc2";
     copyFilenames(filesn);
+}
+
+TEST_GROUP_BASE(TypesTest, RedsTest)
+{
+    char        *e_type;
+
+
+    void setup() {
+        RedsTest::setup();
+        sentence = "\nType expected: ";
+    };
+    void copyTypes(const char *types) {
+        size_t length = strlen(types);
+
+        e_type = (char *) malloc(length + 1);
+        strcpy(e_type, types);
+    };
+
+    std::string typetostr(char type) {
+        const char *TYPES[4] = {
+            "INPUT",
+	        "OUTPUT",
+	        "APPEND",
+	        "HEREDOC"
+        };
+        std::string res; 
+        if (type & EXPAND)
+        {
+            type ^= EXPAND;
+            res = "EXPAND - ";
+        }
+        res += TYPES[(int)type];
+        return (res); 
+    };
+    void teardown() {
+        ctor(&cmdstr, testvalue.c_str());
+        cmd = get_cmd(cmdstr); 
+        for (int i = 0; e_type[i]; i++) {
+            message = sentence + typetostr(e_type[i]) + "\n" "Value obtained: " + typetostr(cmd->reds[i].type) + "\n";
+            CHECK_TEXT(e_type[i] == cmd->reds[i].type, message.c_str());
+        }
+        free(e_type);
+        RedsTest::teardown();       
+    };
+};
+
+TEST(TypesTest, ExpandInput)
+{
+    const char types[] = {
+        INPUT | EXPAND,
+        OUTPUT, 
+        0
+    };
+    testvalue = "echo ExpandInput <$uno >\"dos\"";
+    copyTypes(types);
 }
