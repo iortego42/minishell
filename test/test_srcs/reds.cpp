43d166ac4e0bc7ac30790d634651b6481b51ec34
nachh <iortego42madrid@gmail.com>
2024-03-24 21:42:48 +0100

    Nuevos test probados y pasados

--- test/test_srcs/reds.cpp
+++ test/test_srcs/reds.cpp
@@ -27,8 +27,8 @@ TEST_BASE(RedsTest)
 
 TEST_GROUP_BASE(RedsCount, RedsTest)
 {
-    int         expected_reds;
-    t_string    expected_cmdstr; 
+    int         e_reds;
+    t_string    e_cmdstr; 
 
     void setup() {
         RedsTest::setup();
@@ -39,40 +39,105 @@ TEST_GROUP_BASE(RedsCount, RedsTest)
         ctor(&cmdstr, testvalue.c_str());
         message = sentence + testvalue;
         cmd = get_cmd(cmdstr);
-        for (int i = 0; i < cmd->reds_c; printnl(cmd->reds[i++].filename));
-        printnl(cmd->cmd);
-        CHECK_TEXT(cmd->reds_c == expected_reds, message.c_str());
-        CHECK_TEXT(str_cmp(expected_cmdstr, cmd->cmd) == -1, message.c_str());
-        dtor(&expected_cmdstr);
+        // for (int i = 0; i < cmd->reds_c; printnl(cmd->reds[i++].filename));
+        CHECK_TEXT(cmd->reds_c == e_reds, message.c_str());
+        CHECK_TEXT(str_cmp(e_cmdstr, cmd->cmd) == -1, message.c_str());
+        dtor(&e_cmdstr);
         RedsTest::teardown();
     }
 };
 
-TEST(RedsCount, OneRedSC)
+TEST(RedsCount, OneRedSQ)
 {
-    expected_reds = 1;
-    ctor(&expected_cmdstr, "echo OneRedSC ");
-    testvalue = "echo OneRedSC >'esto'";
+    e_reds = 1;
+    ctor(&e_cmdstr, "echo OneRedSQ ");
+    testvalue = "echo OneRedSQ >'esto'";
     // testvalue = "echo hola >'esto es una red'";
 }
 
-TEST(RedsCount, OneRedDC)
+TEST(RedsCount, OneRedDQ)
 {
-    expected_reds = 1;
-    ctor(&expected_cmdstr, "echo OneRedDC ");
-    testvalue = "echo OneRedDC >\"doubequotees\"";
+    e_reds = 1;
+    ctor(&e_cmdstr, "echo OneRedDQ ");
+    testvalue = "echo OneRedDQ >\"doubequotees\"";
 }
 
 TEST(RedsCount, TwoRedSimple)
 {
-    expected_reds = 2;
-    ctor(&expected_cmdstr, "echo TwoRedSimple  ");
+    e_reds = 2;
+    ctor(&e_cmdstr, "echo TwoRedSimple  ");
     testvalue = "echo TwoRedSimple >>\"doubequotees\" >>EOF";
 }
 
 TEST(RedsCount, MultiRedsMix)
 {
-    expected_reds = 4;
-    ctor(&expected_cmdstr, "echo MultiRedsMix    ");
+    e_reds = 4;
+    ctor(&e_cmdstr, "echo MultiRedsMix    ");
     testvalue = "echo MultiRedsMix >>\"append\" <infile <<heredoc >'hola'";
+}
+
+//
+//  FILENAME TEST
+//
+
+TEST_GROUP_BASE(FilenameTest, RedsTest)
+{
+    t_string    *e_filename;
+    char        **filenames;
+    int         files;
+    void setup() {
+        RedsTest::setup();
+        sentence = "Expected Values: ";
+    };
+
+    void copyFilenames(const char *filesn[])
+    {
+        filenames = (char **)malloc(sizeof(char *) * (unsigned long)(files + 1));
+        for (int i = 0; filesn[i]; i++) {
+            filenames[i] = (char *)malloc(strlen(filesn[i]) + 1);
+            strcpy(filenames[i], filesn[i]);
+        }
+        filenames[files] = NULL;
+    };
+
+    void teardown() {
+        ctor(&cmdstr, testvalue.c_str());
+        cmd = get_cmd(cmdstr); 
+        e_filename = (t_string *)malloc(sizeof(t_string) * (unsigned long)(files + 1));
+        for (int i = 0; i < files; ctor(&e_filename[i], filenames[i]), i++);
+        e_filename[files] = NULL;
+        for (int i = 0; e_filename[i] != NULL; i++)
+        {
+            message = sentence + e_filename[i]->data + "\n" "Value obtained: " + cmd->reds[i].filename->data + "\n";
+            CHECK_TEXT(str_cmp(e_filename[i], cmd->reds[i].filename), message.c_str());
+        }
+        for (int i = 0; e_filename[i] != NULL; dtor(&e_filename[i++]));
+        free(e_filename);
+        for (int i = 0; filenames[i]; free(filenames[i++]));
+        free(filenames);
+        RedsTest::teardown();
+    };
+};
+
+TEST(FilenameTest, OneOutputNoQ)
+{ 
+    const char *filesn[] = {"hola", NULL};
+    files = 1;
+    testvalue = "echo OneOutputNoQ >hola";
+    copyFilenames(filesn);
+}
+
+TEST(FilenameTest, Various)
+{
+    const char *filesn[] = {
+        "outputfile1",
+        "'simpleherdoc'",
+        "\"doubleappend\"",
+        "\"doubleinput\"",
+        "heredoc2",
+        NULL
+    };
+    files = 5;
+    testvalue = "echo Various >outputfile1 <<'simpleheredoc' >>\"doubleappend\" <\"doubleinput\" <<heredoc2";
+    copyFilenames(filesn);
 }
