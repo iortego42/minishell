461e482e1fc9dee35a081e38167901c636835261
nachh <iortego42madrid@gmail.com>
2024-05-29 21:15:05 +0200

    Prototipo de la funcion get_token

--- src/parser/token.c
+++ src/parser/token.c
@@ -49,8 +49,9 @@ void    add_token(t_token *list, struct s_token token)
     if ((*list)->right == NULL)
         // return (clear_tokens());
         return (clean_tokens(list));
+    token.right = NULL;
+    token.left = (*list);
     *(*list)->right = token;
-    (*list)->right->left = list;
 }
 
 // comprobar fallos de memoria y segfaults
@@ -80,11 +81,40 @@ void    remove_token(t_token *list)
     free(aux);
 }
 
+void    get_token(t_DFA *l)
+{
+    size_t          start;
+    size_t          end;
+    t_state         status;
+    t_string        new_cur;
+    struct s_token  token;
+
+    end = 0;
+    status = l->state;
+    // necesario actualizar la marca en función del estado
+    // (comillas dobles, comillas simples o word)
+    start = l->cursor;
+    while (l->cursor->start + end < l->cursor->end)
+    {
+        eval_char(l, get(l->cursor, end));
+        if (l->prev_state == status && l->state != status)
+            break;
+        end++;
+    }
+    token.str = str_ncpy(l->cursor, end);
+    //revisar el -1, puede que no haga falta
+    new_cur = str_rmpos(l->cursor, start, start + end - 1);
+    new_cur->start = start;
+    dtor(&l->cursor);
+    l->cursor = new_cur;
+}
+
 t_token *get_token_list(t_DFA   *l)
 {
     t_token *list;
 
     // (*l->transactions)[][] = token++;
     eval(l);
+
 
 }
